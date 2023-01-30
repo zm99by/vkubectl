08817f72c4e082ba90f0187d9f2bd2d0e566d168
Uladzimir Sakhonchik <zm99by@gmail.com>
2023-01-30 23:21:11 +0100

    add simple logic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 type Pod struct {
@@ -14,13 +15,33 @@ type Pod struct {
 
 func main() {
 	pods := []Pod{
-		{"my-pod-1", "1/1", "Running", "0", "14m"},
-		{"my-pod-2", "1/1", "Running", "0", "44m"},
-		{"my-pod-3", "0/1", "Pending", "0", "144m"},
+		//	{"my-pod-1", "1/1", "Running", "0", "14m"},
+		//	{"my-pod-2", "1/1", "Running", "0", "44m"},
+		//	{"my-pod-3", "0/1", "Pending", "0", "144m"},
 	}
+
+	for _, pod := range pods {
+		fmt.Printf("%-25s %-20s %-15s %-10s %-5s\n", pod.Name, pod.READY, pod.STATUS, pod.RESTARTS, pod.AGE)
+	}
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: add-pod <name> <status> <ready> <restarts> <age>")
+		os.Exit(1)
+	}
+
+	newPod := Pod{
+		Name:     os.Args[1],
+		READY:    "1/1",
+		STATUS:   "Running",
+		RESTARTS: "0",
+		AGE:      "14m",
+	}
+
+	pods = append(pods, newPod)
 
 	fmt.Printf("%-25s %-20s %-15s %-10s %-5s\n", "NAME", "READY", "STATUS", "RESTARTS", "AGE")
 	for _, pod := range pods {
 		fmt.Printf("%-25s %-20s %-15s %-10s %-5s\n", pod.Name, pod.READY, pod.STATUS, pod.RESTARTS, pod.AGE)
 	}
+
 }
